698a50acdb72bac26a7abf597e12647d9d9c6e10
Eidniuet <wenghongtong27@gmail.com>
2023-07-30 16:55:04 +0800

    a

--- backtracking/Nqueen.py
+++ backtracking/Nqueen.py
@@ -1,6 +1,12 @@
+def check (state, nextrow):
+    nextcol = len(state)
+    return any(abs(state[i]-nextrow) in (0, nextcol-i) for i in range(nextcol)) # check row and 
+
 def queen(n, state):
+    if len(state) == n:
+        return [()]
     ans=[]
     for pos in range(n):
-        if pos != 0:
-            ans = ans +1
+        if not check(state, pos):
+            ans += [(pos,) + result for result in queen(n, state +(pos,))]
     return ans
