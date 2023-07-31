a3c5070fe4ca7b03dbbd364dbe5425a3e6aa5ad0
Eidniuet <140290104+Eidniuet@users.noreply.github.com>
2023-07-31 21:06:50 +0800

    Update Rearrange_String.py

--- Rearrange_String.py
+++ Rearrange_String.py
@@ -1,20 +1,20 @@
 def custom_sort(char):
-    if char.islower():
-        return (0,char)
+    if char.islower(): 
+        return (0,char) # 0 is order , char is variable
     
     elif char.isupper():
         return (1,char)
     
-    elif char.isdigit():
-        if int(char) %2 !=0:
+    elif char.isdigit(): # is digit
+        if int(char) %2 !=0: # odd
             return (2,char)
-        else:
+        else: # even
             return(3,char)
-    else:
+    else: # for other char for specified on above
         return(4,char)
 
 def sort_string(input_string):
-    return "".join(sorted(input_string, key=custom_sort))
+    return "".join(sorted(input_string, key=custom_sort)) #it will separate as each element after sorted so needed ''.join 
 
 input_string = "Sorting1234"
 print(sort_string(input_string))
